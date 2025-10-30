0c731664d75acedcb1577ac092c0e60478891f4c
paulagd04 <paulagd04@educastur.es>
2025-10-30 09:58:09 +0100

    Add files via upload

---
+++ carpeta/sketch (1).js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);       // 
+  stroke (200, 0, 0) ;
+  strokeWeight (8) ;
+  line(50, 50, 50, 300); // 
+  stroke (200, 0, 0) ;
+  strokeWeight (8) ;
+  line (50, 50, 200, 50) ;
+  line (200, 50, 200, 150) ;
+  line (200, 150, 50, 150) ;
+
+
+}
